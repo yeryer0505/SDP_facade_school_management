1517b7763a3593b439b2859dfa0333043007f783
Yerassyl <yerasil.yerkin@icloud.com>
2025-10-16 12:54:44 +0500

    a

--- src/school_facade/Accountant.java
+++ src/school_facade/Accountant.java
@@ -1,8 +1,32 @@
 package school_facade;
 
 public class Accountant implements IJob {
+    private String name;
+    private String action;
+
+    public Accountant() {
+        this.name = "Accountant";
+        this.action = "Managing payments";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
     @Override
     public void doWork() {
-        System.out.println("Accountant: Managing payments");
+        System.out.println(name + ": " + action);
     }
 }
