ab5de4eb305386746f939321eb1baf647efb39e8
Yerassyl <yerasil.yerkin@icloud.com>
2025-10-16 13:15:10 +0500

    a

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-package school_facade;
+import school_facade.SchoolFacade;
 
 public class Main {
     public static void main(String[] args) {
--- src/school_facade/SchoolFacade.java
+++ src/school_facade/SchoolFacade.java
@@ -5,17 +5,72 @@ public class SchoolFacade {
     private Accountant accountant;
     private Secretary secretary;
 
+    private String name;
+    private String startMessage;
+    private String endMessage;
+
     public SchoolFacade() {
         this.teacher = new Teacher();
         this.accountant = new Accountant();
         this.secretary = new Secretary();
+        this.name = "SchoolFacade";
+        this.startMessage = "Starting school day";
+        this.endMessage = "Day successfully started!";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartMessage() {
+        return startMessage;
+    }
+
+    public void setStartMessage(String startMessage) {
+        this.startMessage = startMessage;
+    }
+
+    public String getEndMessage() {
+        return endMessage;
+    }
+
+    public void setEndMessage(String endMessage) {
+        this.endMessage = endMessage;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Accountant getAccountant() {
+        return accountant;
+    }
+
+    public void setAccountant(Accountant accountant) {
+        this.accountant = accountant;
+    }
+
+    public Secretary getSecretary() {
+        return secretary;
+    }
+
+    public void setSecretary(Secretary secretary) {
+        this.secretary = secretary;
     }
 
     public void startSchoolDay() {
-        System.out.println("SchoolFacade: Starting school day");
+        System.out.println(name + ": " + startMessage);
         secretary.doWork();
         teacher.doWork();
         accountant.doWork();
-        System.out.println("SchoolFacade: Day successfully started!");
+        System.out.println(name + ": " + endMessage);
     }
 }
