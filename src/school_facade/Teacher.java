5ff34457a27c604c2e6da27ce3d98573498dc18c
Yerassyl <yerasil.yerkin@icloud.com>
2025-10-16 12:52:18 +0500

    a

--- src/school_facade/Teacher.java
+++ src/school_facade/Teacher.java
@@ -1,8 +1,32 @@
 package school_facade;
 
 public class Teacher implements IJob {
+    private String action;
+    private String name;
+
+    public Teacher() {
+        this.name = "Teacher";
+        this.action = "Teaching students";
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public void doWork() {
-        System.out.println("Teacher: Teaching students");
+        System.out.println(name + ": " + action);
     }
 }
