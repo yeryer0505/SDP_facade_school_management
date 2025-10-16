4246aaa0a46d18cb556d5264922a7e65aa0169df
Yerassyl <yerasil.yerkin@icloud.com>
2025-10-16 12:55:32 +0500

    a

--- src/school_facade/Secretary.java
+++ src/school_facade/Secretary.java
@@ -1,8 +1,32 @@
 package school_facade;
 
 public class Secretary implements IJob {
+    private String name;
+    private String action;
+
+    public Secretary() {
+        this.name = "Secretary";
+        this.action = "Registering new students";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
     @Override
     public void doWork() {
-        System.out.println("Secretary: Registering new students");
+        System.out.println(name + ": " + action);
     }
 }
